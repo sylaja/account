d7178e930995dbb180010fb7a14e1917df55568d
sylaja <sylaja@oxsoftwares.com>
2017-07-24 14:59:17 +0530

    login page is created

---
+++ login.php
@@ -0,0 +1,37 @@
+
+<!--
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+         <link rel="stylesheet" type="text/css" href="newcss.css">
+        <title></title>
+    </head>
+    <body>
+        
+        <br>
+        <br>
+        <div class="st">
+
+            <h3>Login Details</h3>
+            <div id="msg"></div>
+            <form id="myForm">
+
+                <label>Name:<span>*</span></label><input type="text" id="name" name="name" placeholder="Your name">
+                <span class="hidden" id="fname"></span>
+                <br>
+                <label>password:<span>*</span></label><input type="password" id="password" name="name" placeholder="Your password">
+                <div id="submt">
+                    <input type="submit" value="submit">
+                </div>
+            </form>
+
+        </div>
+       
+    </body>
+</html>
