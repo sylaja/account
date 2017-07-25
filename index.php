6840a4414d1ca0819e6f8e18cf5c8ffdf840228e
sylaja <sylaja@oxsoftwares.com>
2017-07-25 17:55:57 +0530

    displayed tables

--- index.php
+++ index.php
@@ -11,44 +11,37 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        
-        <br>
-        <br>
         <div class="st">
+            <div id="bt">
+            <a href="registration.php"><input type="submit" id="sub1" value="register"></a>
+            <a href="login.php"><input type="submit" id="sub2" value="login"></a>
+            </div>
+        <div id="tb">
+                <h2>Our Registered Users</h2>
+            <?php
+            $db = mysqli_connect('localhost', 'root', 'admin@123', 'db2');
+            $query = "SELECT name,email,address FROM users";
+            $result = mysqli_query($db, $query);
 
-            <h3>Registration* Details</h3>
-            <div id="msg"></div>
-            <form id="myForm">
+            echo "<table border='1' width='100%' border-collapse='collapse'>
+                <tr>
+                <th>Username</th>
+                <th>Email</th>
+                <th>City</th>
+                </tr>";
 
-                <label>Name:<span>*</span></label><input type="text" id="name" name="name" placeholder="Your name">
-                <span class="hidden" id="fname"></span>
-                <br>
-                <label>Email:<span>*</span></label><input type="text" id="email" name="email" placeholder="Your email id">
-                <span class="hidden" id="mail"></span>
-                <br>
-                <label>Mobile:<span>*</span></label><input type="tel" id="mobile" name="mobile" placeholder="Your mobile number">
-                <span class="hidden" id="nomobile"></span>
-                <br>
+            while ($row = mysqli_fetch_array($result)) {
+                echo "<tr>";
+                echo "<td>" . $row['name'] . "</td>";
+                echo "<td>" . $row['email'] . "</td>";
+                echo "<td>" . $row['address'] . "</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
 
-               
-                <br>
-                <label> Gender:<span>*</span></label><input type="radio" name="gender" value="male">Male
-                <input type="radio" name="gender" value="female">Female 
-                <span class="hidden" id="gen"></span>
-                <br>
-               
-                <label>Address:</label>
-                <div id="feedbck"><br> <textarea id="feedback" name="feedback" rows="3" cols="15" placeholder="Enter Some text"></textarea>
-                    <span class="hidden" id="fbd"></span>
-                </div>
-                <br>
-                <br>
-                <div id="submt">
-                    <input type="submit" value="submit">
-                </div>
-            </form>
-
+            mysqli_close($con);
+            ?>
         </div>
-       
+        </div>
     </body>
 </html>
