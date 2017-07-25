629ae7ceb827dbc99b8dfd43b3d2d097cf9a660d
sylaja <sylaja@oxsoftwares.com>
2017-07-25 18:06:09 +0530

    added buttons and displayed message

--- accountpage.php
+++ accountpage.php
@@ -1,15 +1,19 @@
 
-
+<!--
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+ */-->
  <html>
-     <head></head>
+     <head>
+         <title></title>
+     </head>
      <body>
          <header>
              You are logged-in
+             <br>
+             <input type="submit" value="signout">
          </header>
      </body>
  </html>
