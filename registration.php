2d168c1ddad2f8a495adeeb6d611d920af3150f4
sylaja <sylaja@oxsoftwares.com>
2017-07-25 18:01:19 +0530

    file upload tag is given'

--- registration.php
+++ registration.php
@@ -10,7 +10,7 @@
     <head>
         <meta charset="UTF-8">
          <link rel="stylesheet" type="text/css" href="newcss.css">
-         
+          <script src="jquery.js"></script>
           <script src="newjavascript.js"></script>
         <title></title>
     </head>
@@ -22,10 +22,12 @@
 
             <h3>Registration Details</h3>
             <div id="msg"></div>
-            <form id="myForm">
+            <form id="myForm" method="post" action="values.php">
 
                 <label>Name:<span>*</span></label><input type="text" id="name" name="name" placeholder="Your name">
                 <span class="hidden" id="fname"></span>
+                <br>
+                <label>Password:<span>*</span></label><input type="password" id="password" name="password" placeholder="Your password">
                 <br>
                 <label>Email:<span>*</span></label><input type="text" id="email" name="email" placeholder="Your email id">
                 <span class="hidden" id="mail"></span>
@@ -33,23 +35,30 @@
                 <label>Mobile:<span>*</span></label><input type="tel" id="mobile" name="mobile" placeholder="Your mobile number">
                 <span class="hidden" id="nomobile"></span>
                 <br>
+                <label>DOB:<span>*</span></label><input type="date" id="dob" name="dob">
                 
-               
                 <br>
+               
+                
                 <label> Gender:<span>*</span></label><input type="radio" name="gender" value="male">Male
                 <input type="radio" name="gender" value="female">Female 
                 <span class="hidden" id="gen"></span>
                 <br>
                
-                <label>Address:</label>
+                <label>Address:<span>*</span></label>
                 <div id="feedbck"><br> <textarea id="feedback" name="feedback" rows="3" cols="15" placeholder="Enter Some text"></textarea>
                     <span class="hidden" id="fbd"></span>
                 </div>
                 <br>
+                 <input type="file" name="fileToUpload" id="fileToUpload">
+                <br>
                 <br>
                 <div id="submt">
-                    <input type="submit" value="submit">
+                    <input type="submit" value="submit" name="register">
+                    <br>
+                      Already a member? <a href="login.php"> Login </a>
                 </div>
+              
             </form>
 
         </div>
